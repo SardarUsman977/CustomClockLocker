44bf5175070a1bd4f25b8b4085f743d663126484
sohan <sohanzid@gmail.com>
2022-05-06 19:57:17 +0600

    chaged forked project

---
+++ app/src/main/java/lab/lab1.java
@@ -0,0 +1,22 @@
+package lab;
+
+public class lab1 {
+
+    int lab_no;
+
+    lab1(int a)
+    {
+        this.lab_no=a;
+    }
+
+    public void setNo(int num)
+    {
+        lab_no=num;
+
+    }
+    public int getNo()
+    {
+        return lab_no;
+
+    }
+}
